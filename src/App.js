90988708dd692a02c92fa8c89eca5dfe39b1566e
Armen Kurdoyan <knan2599@gmail.com>
2018-10-26 14:01:08 +0400

    Done, everything is working. Changed everyting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
 import Header from './Components/header/Header';
-import Footer from './Components/footer/Footer';
+import BitcoinRate from './Components/footer/Footer';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          isBcRateVisible: false,
+      };
+
+      this.showBcRate = this.showBcRate.bind(this)
+
+  }
+
+  showBcRate() {
+      this.setState({
+          isBcRateVisible: !this.state.isBcRateVisible
+      });
+  }
+
+  changeBtnTxt() {
+      return this.state.isBcRateVisible ?
+          "Hide Bitcoin rate" :
+          "Show Bitcoin rate";
+  }
+
   render() {
-    return (
-      <div className="App">
-        <Header />
-        <Footer />
-      </div>
-    );
+      return (
+          <div className="App">
+              <h1>Rate calculator</h1>
+              <Header/>
+              <button
+                  onClick={this.showBcRate}
+                  className="App-show-hide"
+                  type="button">
+                  {this.changeBtnTxt()}
+              </button>
+              {this.state.isBcRateVisible && <BitcoinRate/>}
+          </div>
+      );
   }
 }
 
